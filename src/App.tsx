8f7215b596611bcafdb398801a5abcc265ff51ac
Rudex Tede <rudexjuniortede@gmail.com>
2025-09-06 01:30:20 +0100

    Text Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
 import Button from "./components/Button"
 
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [variant, setVariant] = useState<"primary" | "secondary">("primary")
+
+  const handlePrimaryClick = () => {
+    setCount(prev => prev + 1)
+  }
+  const handleSecondaryClick = () => {
+    if (variant === "primary") {
+      setVariant("secondary");
+    } else {
+      setVariant("primary");
+    }
+  }
   return (
     <>
       {/* Buttons */}
       <div className="flex items-center justify-center m-40 gap-5">
-        <Button>primary</Button>
-        <Button variant="secondary">secondary</Button>
+        <Button onClick={handlePrimaryClick}>primary {count}</Button>
+        <Button variant={variant} onClick={handleSecondaryClick}>secondary</Button>
       </div>
     </>
   )
